7cb2873ca216b790a2db0f2f01f1767c004c499a
Manfred Stienstra <manfred@fngtps.com>
2023-01-31 19:41:08 +0100

    Make sure USERNOTICE is parsed correctly.

--- spec/dorsum/twitch/chat/message_spec.cr
+++ spec/dorsum/twitch/chat/message_spec.cr
@@ -16,8 +16,8 @@ describe Dorsum::Twitch::Chat::Message do
       message.ansi_color.ansi_code.should eq(231)
       message.badges.should eq(["subscriber/2012", "sub-gifter/250"])
       message.badge.should eq("◇")
-
       message.sent_at.should eq Time.unix_ms(1640987950054)
+
       message.formatted_ban_duration.should eq "permanent"
     end
 
@@ -33,6 +33,25 @@ describe Dorsum::Twitch::Chat::Message do
       message.message.should eq "chadow_of_dragnoir"
 
       message.formatted_ban_duration.should eq "5s"
+    end
+
+    it "parses USERNOTICE" do
+      text = "@badge-info=subscriber/26;badges=subscriber/12,bits/5000;color=#DE9027;display-name=teammanfred;emotes=emotesv2_44e58a38550446398c457885e94aab61:0-10;flags=;id=adaff326-8af5-42fd-acb5-91ceb6dd5a8e;login=teammanfred;mod=0;msg-id=resub;msg-param-cumulative-months=26;msg-param-months=0;msg-param-multimonth-duration=0;msg-param-multimonth-tenure=0;msg-param-should-share-streak=0;msg-param-sub-plan-name=Channel\\sSubscription\\s(Tomato\\sGaming);msg-param-sub-plan=1000;msg-param-was-gifted=false;room-id=43784278;subscriber=1;system-msg=teammanfred\\ssubscribed\\sat\\sTier\\s1.\\sThey've\\ssubscribed\\sfor\\s26\\smonths!;tmi-sent-ts=1675189772827;user-id=131842899;user-type= :tmi.twitch.tv USERNOTICE #tomato :tomatoArise"
+      message = Dorsum::Twitch::Chat::Message.new(text)
+      message.annotations.should eq "badge-info=subscriber/26;badges=subscriber/12,bits/5000;color=#DE9027;display-name=teammanfred;emotes=emotesv2_44e58a38550446398c457885e94aab61:0-10;flags=;id=adaff326-8af5-42fd-acb5-91ceb6dd5a8e;login=teammanfred;mod=0;msg-id=resub;msg-param-cumulative-months=26;msg-param-months=0;msg-param-multimonth-duration=0;msg-param-multimonth-tenure=0;msg-param-should-share-streak=0;msg-param-sub-plan-name=Channel\\sSubscription\\s(Tomato\\sGaming);msg-param-sub-plan=1000;msg-param-was-gifted=false;room-id=43784278;subscriber=1;system-msg=teammanfred\\ssubscribed\\sat\\sTier\\s1.\\sThey've\\ssubscribed\\sfor\\s26\\smonths!;tmi-sent-ts=1675189772827;user-id=131842899;user-type="
+      message.source.should eq ":tmi.twitch.tv"
+      message.command.should eq "USERNOTICE"
+      message.arguments.should eq "#tomato"
+      message.channel.should eq "tomato"
+      message.message.should eq "tomatoArise"
+
+      message.display_name.should eq("teammanfred")
+      message.ansi_color.ansi_code.should eq(179)
+      message.badges.should eq(["subscriber/12", "bits/5000"])
+      message.badge.should eq("◇")
+      message.sent_at.should eq Time.unix_ms(1675189772827)
+
+      message.formatted_ban_duration.should eq "permanent"
     end
   end
 end
